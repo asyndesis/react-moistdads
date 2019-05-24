a703d3e7e2ab8662a7d357ea84d3615d06793e37
Trevor Bonomi <trevorbonomi@gmail.com>
2019-05-24 15:41:26 -0400

    fix

--- server/config/config.mjs
+++ server/config/config.mjs
@@ -10,11 +10,11 @@ export default {
     maxUploadSize: 20963164
   },
   production : {
-    auth: false,
+    auth: false, //disable token in routes/middleware
     jwtSecret: 'arfarfarfarf123123farts',
     webServerPort : 4100,
-    mongoDatabaseUri : "mongodb://xxxxx/moistdads",
-    socketPort: 'o_o',
+    mongoDatabaseUri : "mongodb://localhost:27017/moistdads",
+    socketPort: 'o_o?', /* update socket server file when you find out how to pass a socket */
     uploadDirectory: './public/uploads',
     scratchDirectory: './public/scratch',
     maxUploadSize: 20963164
